92922e55be4047d6227c5b4090be708b8ccaf256
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-06-04 15:37:53 +0500

    environment setup

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    'production-dependencies': ['phaser'],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist',
+    filename: '[name].js',
+    chunkFilename: 'production-dependencies.bundle.js',
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'index.html'),
+          to: path.resolve(__dirname, 'dist')
+        },
+        {
+          from: path.resolve(__dirname, 'assets', '**', '*'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
+    new HtmlWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets',
+          },
+        },
+      },
+    ],
+  },
+};
