51672d6383ff74038fc6fd03db873dbf263a1f13
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-06-26 11:09:54 +0500

    added units

---
+++ src/unit.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser';
+
+const Unit = new Phaser.Class({
+  Extends: Phaser.GameObjects.Sprite,
+
+  initialize: function Unit(scene, x, y, texture, frame, type, hp, damage) {
+    Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame);
+    this.type = type;
+    this.maxHp = this.hp = hp;
+    this.damage = damage;
+  },
+
+  attack(target) {
+    target.takeDamage(this.damage);
+    this.scene.events.emit(`Message ${this.type} attacks ${target.type} for ${this.damage} damage`);
+  },
+
+  takeDamage(damage) {
+    this.hp -= damage;
+    if (this.hp <= 0) {
+      this.hp = 0;
+      this.alive = false;
+    }
+  },
+});
+
+export { Unit as default };
